62887895e33ad37df75355bb1b978f679013b93c
y.hidaka <y.hidaka@>
2025-03-27 15:19:40 +0900

    build update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,27 +2,88 @@
 import './App.css';
 
 function App() {
+
+  const frontEndTechs = [
+    {
+      name: 'HTML',
+      description: '学習中',
+    },
+    {
+      name: 'CSS',
+      description: '学習中',
+    },
+    {
+      name: 'JavaScript',
+      description: '学習中',
+    },
+  ];
+
+  const programingTechs = [
+    {
+      name: 'VB.net',
+      description: '学習中',
+    },
+    {
+      name: 'Python',
+      description: '学習中',
+    },
+    {
+      name: 'JavaScript',
+      description: '学習中',
+    },
+  ];
+
+  const frameworkTechs = [
+    {
+      name: 'React',
+      description: '学習中',
+    },
+    {
+      name: 'flet',
+      description: '学習中',
+    },
+    {
+      name: 'Selenium',
+      description: '学習中',
+    },
+  ];
+
   return (
     <div className="App">
-  <h1>学んだ技術に関するアウトプットスペース</h1>
-  <h2>技術スタック</h2>
-  <h3>フロントエンド技術</h3>
-  <ul>
-    <li>HTML : 学習中</li>
-    <li>CSS : 学習中</li>
-  </ul>
-  <h3>プログラミング言語</h3>
-  <ul>
-    <li>VB.net : 学習中</li>
-    <li>Python : 学習中</li>
-  </ul>
-  <h3>フレームワーク</h3>
-  <ul>
-    <li>Selenium : 学習中</li>
-    <li>flet : 学習中</li>
-    <li>react : 学習中</li>
-  </ul>
-  </div>
+
+      <h1>学んだ技術に関するアウトプットスペース</h1>
+      <h2>技術スタック</h2>
+      <h3>フロントエンド技術</h3>
+
+      <div className="feTech-list">
+        {frontEndTechs.map((tech, index) => (
+        <div key={index} className="feTech-item">
+          <p>{tech.name}: {tech.description}</p>
+        </div>
+        ))}
+      </div>
+
+      <h3>プログラミング言語</h3>
+
+      <div className="pgTech-list">
+        {programingTechs.map((tech, index) => (
+        <div key={index} className="pgTech-item">
+          <p>{tech.name}: {tech.description}</p>
+        </div>
+        ))}
+      </div>
+
+      <h3>フレームワーク</h3>
+
+      <div className="fwTech-list">
+        {frameworkTechs.map((tech, index) => (
+        <div key={index} className="fwTech-item">
+          <p>{tech.name}: {tech.description}</p>
+        </div>
+        ))}
+      </div>
+
+    </div>
   );
 }
 
