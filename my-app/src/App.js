07b03c50b23da89659abcda7c0fc183a4b2cb793
y.hidaka <y.hidaka@>
2025-03-27 13:19:33 +0900

    change contents

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,6 +18,25 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+  <h1>学んだ技術に関するアウトプットスペース</h1>
+  <h2>技術スタック</h2>
+  <h3>フロントエンド技術</h3>
+  <ul>
+    <li>HTML : 学習中</li>
+    <li>CSS : 学習中</li>
+  </ul>
+  <h3>プログラミング言語</h3>
+  <ul>
+    <li>VB.net : 学習中</li>
+    <li>Python : 学習中</li>
+  </ul>
+  <h3>フレームワーク</h3>
+  <ul>
+    <li>Selenium : 学習中</li>
+    <li>flet : 学習中</li>
+  </ul>
+</body>
     </div>
   );
 }
