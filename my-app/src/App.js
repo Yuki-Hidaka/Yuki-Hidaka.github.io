438062b73533349bc1f5f5d580d920b3afb1dad1
y.hidaka <y.hidaka@>
2025-03-27 14:20:31 +0900

    build

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <body>
   <h1>学んだ技術に関するアウトプットスペース</h1>
   <h2>技術スタック</h2>
   <h3>フロントエンド技術</h3>
@@ -35,9 +20,9 @@ function App() {
   <ul>
     <li>Selenium : 学習中</li>
     <li>flet : 学習中</li>
+    <li>react : 学習中</li>
   </ul>
-</body>
-    </div>
+  </div>
   );
 }
 
